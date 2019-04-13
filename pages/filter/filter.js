b834bf1092f6cc8b1eb621db445baac4160d1c58
张睿珂 <zhangruike@zhangruikedeMacBook-Pro.local>
2019-04-13 10:30:43 +0800

    date_filter
    add date filter

--- pages/filter/filter.js
+++ pages/filter/filter.js
@@ -1,4 +1,6 @@
 // pages/filter/filter.js
+wx.cloud.init()
+const db = wx.cloud.database()
 Page({
 
   /**
@@ -24,27 +26,116 @@ Page({
         })
       }
     })
-    wx.request({
-      url: 'https://www.kashingliu.cn/wechattest/filter.php?timestamp='+options.time,
-      data: {
+    
+      if(options.time == 0){
+        const _ =db.command
+        db.collection('posts').where({
+           datedetail: _.gte(db.serverDate({ offset: -24 * 60 * 60 * 1000 })).and(_.lte(db.serverDate({ offset: 24 * 60 * 60 * 1000 })))
+         
+        }).get({
+          data: {
 
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success(res) {
-        console.log(res)
-        self.setData({
-          currentList: res.data,
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res) {
+            console.log(res)
+            self.setData({
+              currentList: res.data
+            })
+            if (res.data.length == 0) {
+              self.setData({
+                isNone: false
+              })
+            }
+
+          }
         })
-        if(res.data.length == 0) {
-          self.setData({
-            isNone: false
-          })
-        }
+       }
+       else if(options.time == 1){
+        const _ = db.command
+        db.collection('posts').where({
+          datedetail: _.gte(db.serverDate({ offset: -3*24 * 60 * 60 * 1000 })).and(_.lte(db.serverDate({ offset: 24 * 60 * 60 * 1000 })))
 
+        }).get({
+          data: {
+
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res) {
+            console.log(res)
+            self.setData({
+              currentList: res.data
+            })
+            if (res.data.length == 0) {
+              self.setData({
+                isNone: false
+              })
+            }
+
+          }
+        })
       }
-    })
+      else if(options.time == 2){
+        const _ = db.command
+        db.collection('posts').where({
+          datedetail: _.gte(db.serverDate({ offset: -7*24 * 60 * 60 * 1000 })).and(_.lte(db.serverDate({ offset: 24 * 60 * 60 * 1000 })))
+
+        }).get({
+          data: {
+
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res) {
+            console.log(res)
+            self.setData({
+              currentList: res.data
+            })
+            if (res.data.length == 0) {
+              self.setData({
+                isNone: false
+              })
+            }
+
+          }
+        })
+      }
+      else {
+        const _ = db.command
+        db.collection('posts').where({
+          datedetail: _.gte(db.serverDate({ offset: -30*24 * 60 * 60 * 1000 })).and(_.lte(db.serverDate({ offset: 24 * 60 * 60 * 1000 })))
+
+        }).get({
+          data: {
+
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res) {
+            console.log(res)
+            self.setData({
+              currentList: res.data
+            })
+            if (res.data.length == 0) {
+              self.setData({
+                isNone: false
+              })
+            }
+
+          }
+        })
+      }
+       
+      
+    
+    
+    
   },
 
   detailTap: function (e) {
